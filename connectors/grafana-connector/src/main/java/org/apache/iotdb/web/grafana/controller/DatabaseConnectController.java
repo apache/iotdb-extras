3a860d8205ff1ea43f185a333e5d579f5f4eabd8
Christofer Dutz <cdutz@apache.org>
2024-05-13 17:41:33 +0200

    chore: Some further cleaning up in the poms

--- connectors/grafana-connector/src/main/java/org/apache/iotdb/web/grafana/controller/DatabaseConnectController.java
+++ connectors/grafana-connector/src/main/java/org/apache/iotdb/web/grafana/controller/DatabaseConnectController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.Instant;
--- connectors/grafana-connector/src/main/java/org/apache/iotdb/web/grafana/interceptor/LoginInterceptor.java
+++ connectors/grafana-connector/src/main/java/org/apache/iotdb/web/grafana/interceptor/LoginInterceptor.java
@@ -24,8 +24,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
