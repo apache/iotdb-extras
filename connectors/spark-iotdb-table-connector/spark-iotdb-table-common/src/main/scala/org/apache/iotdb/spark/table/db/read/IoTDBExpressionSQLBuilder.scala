4785b88c37447b54eb5f656bb0fb7fc069e1a864
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2025-03-14 09:24:08 +0800

    IoTDB Table Spark Connector

---
+++ connectors/spark-iotdb-table-connector/spark-iotdb-table-common/src/main/scala/org/apache/iotdb/spark/table/db/read/IoTDBExpressionSQLBuilder.scala
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.spark.table.db.read
+
+import org.apache.iotdb.spark.table.db.IoTDBUtils
+import org.apache.spark.sql.catalyst.util.DateTimeUtils
+import org.apache.spark.sql.connector.expressions.filter.{And, Not, Or, Predicate}
+import org.apache.spark.sql.connector.expressions.{Expression, GeneralScalarExpression, Literal, NamedReference}
+import org.apache.spark.sql.types._
+
+class IoTDBExpressionSQLBuilder {
+
+  def build(predicate: Predicate): String = {
+    s"(${buildIoTDBExpressionSQL(predicate)})"
+  }
+
+  private def buildIoTDBExpressionSQL(expression: Expression): String = {
+    expression match {
+      case literal: Literal[_] => visitLiteral(literal)
+      case namedReference: NamedReference => visitNamedReference(namedReference)
+      case expr: GeneralScalarExpression => visitGeneralScalarExpression(expr)
+      case _ => throw new UnsupportedOperationException("Unsupported push down expression: " + expression)
+    }
+  }
+
+  private def visitLiteral(literal: Literal[_]): String = {
+    literal.dataType() match {
+      case StringType => s"'${literal.value().toString.replace("'", "''")}'"
+      case BinaryType => IoTDBUtils.getIoTDBHexStringFromByteArray(literal.value().asInstanceOf[Array[Byte]])
+      case DateType => s"CAST('${DateTimeUtils.toJavaDate(Integer.parseInt(literal.value().toString))}' as DATE)"
+      case ShortType | IntegerType | ByteType | LongType | BooleanType | FloatType | DoubleType => literal.value().toString
+      case _ => throw new UnsupportedOperationException("Unsupported push down literal type: " + literal.dataType())
+    }
+  }
+
+  private def visitNamedReference(namedRef: NamedReference): String = {
+    IoTDBUtils.getIoTDBColumnIdentifierInSQL(namedRef.toString, true)
+  }
+
+  private def visitAlwaysFalse(): String = {
+    "FALSE"
+  }
+
+  private def visitAlwaysTrue(): String = {
+    "TRUE"
+  }
+
+  private def visitOr(or: Or): String = {
+    s"(${buildIoTDBExpressionSQL(or.left())}) OR (${buildIoTDBExpressionSQL(or.right())})"
+  }
+
+  private def visitAnd(and: And): String = {
+    s"(${buildIoTDBExpressionSQL(and.left())}) AND (${buildIoTDBExpressionSQL(and.right())})"
+  }
+
+  private def visitNot(not: Not): String = {
+    s"NOT (${buildIoTDBExpressionSQL(not.child())})"
+  }
+
+  private def visitGeneralScalarExpression(expr: GeneralScalarExpression): String = {
+    // <=> is unsupported
+    expr.name() match {
+      case "IS_NULL" => visitIsNull(expr)
+      case "IS_NOT_NULL" => visitIsNotNull(expr)
+      case "STARTS_WITH" => visitStartsWith(expr)
+      case "ENDS_WITH" => visitEndsWith(expr)
+      case "CONTAINS" => visitContains(expr)
+      case "IN" => visitIn(expr)
+      case "=" => visitEqualTo(expr)
+      case "<>" => visitNotEqualTo(expr)
+      case "<" => visitLess(expr)
+      case "<=" => visitLessOrEqual(expr)
+      case ">" => visitGreater(expr)
+      case ">=" => visitGreaterOrEqual(expr)
+      case "AND" => visitAnd(expr.asInstanceOf[And])
+      case "OR" => visitOr(expr.asInstanceOf[Or])
+      case "NOT" => visitNot(expr.asInstanceOf[Not])
+      case "ALWAYS_TRUE" => visitAlwaysTrue()
+      case "ALWAYS_FALSE" => visitAlwaysFalse()
+      case _ => throw new UnsupportedOperationException("Unsupported push down expression: " + expr)
+    }
+  }
+
+  private def visitIsNull(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} IS NULL"
+  }
+
+  private def visitIsNotNull(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} IS NOT NULL"
+  }
+
+  private def visitStartsWith(expr: Expression): String = {
+    val leftExpr = buildIoTDBExpressionSQL(expr.children()(0))
+    val rightExpr = buildIoTDBExpressionSQL(expr.children()(1))
+    s"starts_with(${leftExpr}, ${rightExpr})"
+  }
+
+  private def visitEndsWith(expr: Expression): String = {
+    val leftExpr = buildIoTDBExpressionSQL(expr.children()(0))
+    val rightExpr = buildIoTDBExpressionSQL(expr.children()(1))
+    s"ends_with(${leftExpr}, ${rightExpr})"
+  }
+
+  private def visitContains(expr: Expression): String = {
+    if (expr.children()(1).isInstanceOf[NamedReference]) {
+      throw new UnsupportedOperationException("Unsupported push down expression: contains non constant string")
+    }
+    val leftExpr = buildIoTDBExpressionSQL(expr.children()(0))
+    val rightExpr = buildIoTDBExpressionSQL(expr.children()(1))
+    s"$leftExpr LIKE '%${rightExpr.substring(1, rightExpr.length - 1)}%'"
+  }
+
+  private def visitIn(expr: Expression): String = {
+    val expressions = expr.children()
+    val leftExpr = buildIoTDBExpressionSQL(expressions(0))
+    val rightExpr = expressions.slice(1, expressions.length).map(buildIoTDBExpressionSQL).mkString(",")
+    s"$leftExpr IN ($rightExpr)"
+  }
+
+  private def visitEqualTo(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} = ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+  private def visitNotEqualTo(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} != ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+  private def visitLess(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} < ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+  private def visitLessOrEqual(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} <= ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+  private def visitGreater(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} > ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+  private def visitGreaterOrEqual(expr: Expression): String = {
+    s"${buildIoTDBExpressionSQL(expr.children()(0))} >= ${buildIoTDBExpressionSQL(expr.children()(1))}"
+  }
+
+}
---
+++ connectors/spark-iotdb-table-connector/spark-iotdb-table-common/src/main/scala/org/apache/iotdb/spark/table/db/read/IoTDBScanBuilder.scala
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.spark.table.db.read
+
+import org.apache.iotdb.spark.table.db.IoTDBOptions
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.connector.expressions.filter.Predicate
+import org.apache.spark.sql.connector.read._
+import org.apache.spark.sql.types.StructType
+
+import java.util
+
+/**
+ * IoTDBScanBuilder is responsible for constructing an IoTDBScan with
+ * support for predicate push-down, column pruning, offset, and limit.
+ *
+ * @param options The IoTDB connection and query options.
+ * @param schema The full schema of the table.
+ */
+class IoTDBScanBuilder(options: IoTDBOptions, schema: StructType) extends ScanBuilder
+    with SupportsPushDownRequiredColumns
+    with SupportsPushDownV2Filters
+    with SupportsPushDownOffset
+    with SupportsPushDownLimit
+    with Logging {
+
+  private var supportedFilters: Array[Predicate] = Array.empty
+  private var pushDownFilterStrings: Array[String] = Array.empty
+  private var requiredColumns: StructType = schema
+  private var pushDownOffset: Int = -1
+  private var pushDownLimit: Int = -1
+
+  override def build(): Scan = {
+    new IoTDBScan(options, requiredColumns, pushDownFilterStrings, pushDownOffset, pushDownLimit)
+  }
+
+  override def pruneColumns(requiredSchema: StructType): Unit = {
+    if (requiredSchema.nonEmpty) {
+      val fields = schema.fields.filter(
+        field => requiredSchema.fieldNames.contains(field.name)
+      )
+      requiredColumns = StructType(fields)
+    } else {
+      requiredColumns = schema
+    }
+  }
+
+  override def pushOffset(offset: Int): Boolean = {
+    pushDownOffset = offset
+    true
+  }
+
+  override def pushLimit(limit: Int): Boolean = {
+    pushDownLimit = limit
+    true
+  }
+
+  override def pushPredicates(predicates: Array[Predicate]): Array[Predicate] = {
+    val compiledPredicates = new util.ArrayList[String]()
+    val builder = new IoTDBExpressionSQLBuilder
+    val (supported, unsupported) = predicates.partition(predicate => {
+      try {
+        val sql = builder.build(predicate)
+        compiledPredicates.add(sql)
+        true
+      } catch {
+        case e: Exception => {
+          logDebug(s"Predicate push-down failed for: $predicate, reason: ${e.getMessage}")
+          false
+        }
+      }
+    })
+    pushDownFilterStrings = compiledPredicates.toArray(new Array[String](compiledPredicates.size()))
+    supportedFilters = supported
+    unsupported
+  }
+
+  override def pushedPredicates(): Array[Predicate] = {
+		supportedFilters
+  }
+
+}
