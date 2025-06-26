a75dca05eb6b7f5a07ba14abf5b9e7dc5862b66f
xiaoyekanren <876670773@qq.com>
2025-06-26 11:44:53 +0800

    optimized code

--- iotdb-spring-boot-starter/src/main/java/org/apache/iotdb/config/IoTDBSessionProperties.java
+++ iotdb-spring-boot-starter/src/main/java/org/apache/iotdb/config/IoTDBSessionProperties.java
@@ -34,17 +34,17 @@ public class IoTDBSessionProperties {
   private Integer fetch_size = 1024;
   private long query_timeout_in_ms = 60000L;
   private boolean enable_auto_fetch = true;
-  private int max_retry_count = 60;
-  private long wait_to_get_session_timeout_in_msit = 60000L;
+  private Integer max_retry_count = 60;
+  private long wait_to_get_session_timeout_in_ms = 60000L;
   private boolean enable_compression = false;
   private long retry_interval_in_ms = SessionConfig.RETRY_INTERVAL_IN_MS;
   private boolean use_ssl = false;
   private String trust_store;
   private String trust_store_pwd;
-  private int connection_timeout_in_ms;
+  private Integer connection_timeout_in_ms;
   private ZoneId zone_id;
-  private int thrift_default_buffer_size = 1024;
-  private int thrift_max_frame_size = 67108864;
+  private Integer thrift_default_buffer_size = 1024;
+  private Integer thrift_max_frame_size = 67108864;
   private boolean enable_redirection;
   private boolean enable_records_auto_convert_tablet =
       SessionConfig.DEFAULT_RECORDS_AUTO_CONVERT_TABLET;
@@ -105,36 +105,8 @@ public class IoTDBSessionProperties {
     this.fetch_size = fetch_size;
   }
 
-  public Long getQuery_timeout_in_ms() {
+  public long getQuery_timeout_in_ms() {
     return query_timeout_in_ms;
-  }
-
-  public void setQuery_timeout_in_ms(Long query_timeout_in_ms) {
-    this.query_timeout_in_ms = query_timeout_in_ms;
-  }
-
-  public Boolean getEnable_auto_fetch() {
-    return enable_auto_fetch;
-  }
-
-  public void setEnable_auto_fetch(Boolean enable_auto_fetch) {
-    this.enable_auto_fetch = enable_auto_fetch;
-  }
-
-  public Boolean getUse_ssl() {
-    return use_ssl;
-  }
-
-  public void setUse_ssl(Boolean use_ssl) {
-    this.use_ssl = use_ssl;
-  }
-
-  public Integer getMax_retry_count() {
-    return max_retry_count;
-  }
-
-  public void setMax_retry_count(Integer max_retry_count) {
-    this.max_retry_count = max_retry_count;
   }
 
   public void setQuery_timeout_in_ms(long query_timeout_in_ms) {
@@ -157,16 +129,20 @@ public class IoTDBSessionProperties {
     this.use_ssl = use_ssl;
   }
 
-  public void setMax_retry_count(int max_retry_count) {
+  public Integer getMax_retry_count() {
+    return max_retry_count;
+  }
+
+  public void setMax_retry_count(Integer max_retry_count) {
     this.max_retry_count = max_retry_count;
   }
 
-  public long getWait_to_get_session_timeout_in_msit() {
-    return wait_to_get_session_timeout_in_msit;
+  public long getWait_to_get_session_timeout_in_ms() {
+    return wait_to_get_session_timeout_in_ms;
   }
 
-  public void setWait_to_get_session_timeout_in_msit(long wait_to_get_session_timeout_in_msit) {
-    this.wait_to_get_session_timeout_in_msit = wait_to_get_session_timeout_in_msit;
+  public void setWait_to_get_session_timeout_in_ms(long wait_to_get_session_timeout_in_ms) {
+    this.wait_to_get_session_timeout_in_ms = wait_to_get_session_timeout_in_ms;
   }
 
   public boolean isEnable_compression() {
@@ -201,11 +177,11 @@ public class IoTDBSessionProperties {
     this.trust_store_pwd = trust_store_pwd;
   }
 
-  public int getConnection_timeout_in_ms() {
+  public Integer getConnection_timeout_in_ms() {
     return connection_timeout_in_ms;
   }
 
-  public void setConnection_timeout_in_ms(int connection_timeout_in_ms) {
+  public void setConnection_timeout_in_ms(Integer connection_timeout_in_ms) {
     this.connection_timeout_in_ms = connection_timeout_in_ms;
   }
 
@@ -217,19 +193,19 @@ public class IoTDBSessionProperties {
     this.zone_id = zone_id;
   }
 
-  public int getThrift_default_buffer_size() {
+  public Integer getThrift_default_buffer_size() {
     return thrift_default_buffer_size;
   }
 
-  public void setThrift_default_buffer_size(int thrift_default_buffer_size) {
+  public void setThrift_default_buffer_size(Integer thrift_default_buffer_size) {
     this.thrift_default_buffer_size = thrift_default_buffer_size;
   }
 
-  public int getThrift_max_frame_size() {
+  public Integer getThrift_max_frame_size() {
     return thrift_max_frame_size;
   }
 
-  public void setThrift_max_frame_size(int thrift_max_frame_size) {
+  public void setThrift_max_frame_size(Integer thrift_max_frame_size) {
     this.thrift_max_frame_size = thrift_max_frame_size;
   }
 
